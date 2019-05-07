22bebf127d381397d5bb0cbd3ac07f6cc69d583f
Grogan <brian.rg@gmail.com>
2019-05-07 09:52:54 -0600

    2019 Code Sample
    + Basic method chain for printline message
    + TODO

--- Main.java
+++ Main.java
@@ -1,7 +1,39 @@
-
 public class Main {
+// TODO: Expand project structure
 
-    public static void main(String[] args) {
-        System.out.println("Main start :)");
-    }
+	private static String jSdkVer = "";
+
+
+	public static void main(String[] args) {
+
+
+		setJSdkVer();
+
+		System.out.println(getHelloWorldMessage(getJSdk()));
+	}
+
+
+	public static String getHelloWorldMessage(String theAmazingReturnedText) {
+
+		setJSdkVer();
+
+		return "FirstClassTest start - Basic java command line app. Java SDK " + theAmazingReturnedText;
+	}
+
+	private static void setJSdkVer() {
+
+		// critical variable declaration for 12.01
+		int zero = 0;
+		int one = 1;
+		int two = 2;
+
+		jSdkVer = jSdkVer + String.valueOf(one) + String.valueOf(two) + "." + String.valueOf(zero) + String.valueOf(one);
+	}
+
+
+	public static String getJSdk() {
+
+		return jSdkVer;
+	}
+
 }
